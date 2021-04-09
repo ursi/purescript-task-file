9926d479af55a7568032a1360c9e0d8790b64bd7
Mason Mackaman <masondeanm@aol.com>
2021-04-09 14:44:19 -0400

    Update package.nix

--- package.nix
+++ package.nix
@@ -1,8 +1,6 @@
 { ps-pkgs, ps-pkgs-ns, ... }:
   with ps-pkgs;
-  { repo = "https://github.com/ursi/purescript-task-file.git";
-    rev = "d6b5a3eac9148b072c27383b5489ce1b063a7c1f";
-    dependencies =
+  { dependencies =
       [ node-fs
         ps-pkgs-ns.ursi.prelude
         task
